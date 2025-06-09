8f3fa9efde48d3d3d0bfcb8a88030032e0fcb160
manishramanandi <bairagimanish222@gmail.com>
2025-06-09 17:57:51 +0530

    feat: Add all routes

--- main.go
+++ main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
-
-	"fmt"
 
 	"github.com/gorilla/mux"
 )
@@ -15,15 +14,62 @@ type RouteResponse struct {
 }
 
 func main() {
+	log.Println("Starting server...")
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-type", "application/json")
+	log.Println("Setting up routes...")
 
-		json.NewEncoder(w).Encode(RouteResponse{Message: "Hello from the server!"})
-	}).Methods("GET")
+	router.HandleFunc("/register", register).Methods("POST")
+	router.HandleFunc("/login", login).Methods("POST")
+	router.HandleFunc("/projects", createProject).Methods("POST")
+	router.HandleFunc("/projects{id}", updateProjects).Methods("PUT")
+	router.HandleFunc("/projects", getProjects).Methods("GET")
+	router.HandleFunc("/projects{id}", getProject).Methods("GET")
+	router.HandleFunc("/projects{id}", deletProject).Methods("DELETE")
 
-	log.Fatal(http.ListenAndServe(":4269", router))
-	fmt.Println("Server is running on port 4269")
+	fmt.Println("Server is running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	json.NewEncoder(w).Encode(RouteResponse{Message: "From register "})
+}
+
+// login
+
+func login(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	json.NewEncoder(w).Encode(RouteResponse{Message: "From register "})
+}
+
+// create projects
+func createProject(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	json.NewEncoder(w).Encode(RouteResponse{Message: "from createProject"})
+}
+
+// getproject
+func getProject(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	json.NewEncoder(w).Encode(RouteResponse{Message: "from from getproject"})
+}
+
+// getprojects
+func getProjects(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	json.NewEncoder(w).Encode(RouteResponse{Message: "from getProjects"})
+}
+
+// update projects
+func updateProjects(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	json.NewEncoder(w).Encode(RouteResponse{Message: "from updateProject"})
+}
+
+// delete project
+func deletProject(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	json.NewEncoder(w).Encode(RouteResponse{Message: "from deletProject"})
 }
